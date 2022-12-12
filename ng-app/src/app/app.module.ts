253654e8786f2a8047898c5f7775ea9d33f85145
Neto Jocelino <netoj96@live.com>
2022-12-12 09:47:03 -0300

    add home component when access '/'

--- ng-app/src/app/app.module.ts
+++ ng-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     AppHeaderComponent,
   ],
   imports: [
